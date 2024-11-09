cfa8fba858339a5e682324aa5ac76f34ab2699ef
Ricse Anthony <anthony.ricse@unmsm.edu.pe>
2024-11-08 23:51:03 -0500

    fix ( map ): entries

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -2,14 +2,11 @@
 import DashBoardTemplate from "../templates/dashboard";
 import useRate from "../hook/useRate";
 import useClient from "@/app/hook/isClient";
-import { API_URL } from "../lib/utilities/constant";
-import { IRate } from "@/shared/entities/Rate";
 
 const Home = () => {
   const isClient = useClient();
-  const [rates, keys] = useRate(API_URL);
-  console.log("🚀 ~ Home ~ keys:", keys);
-  console.log("🚀 ~ Home ~ isDataRate:", rates);
+  const [rates, keys] = useRate();
+
   return (
     isClient && (
       <DashBoardTemplate>
